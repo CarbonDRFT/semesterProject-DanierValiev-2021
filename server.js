c5330288dfade21f7a011b9989f7c3448826ae3d
CarbonDRFT <67243227+CarbonDRFT@users.noreply.github.com>
2021-12-15 00:07:39 +0100

    update

--- server.js
+++ server.js
@@ -387,6 +387,8 @@ app.use((req, res) => {
   res.redirect("/404");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("listening on port 3000....");
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
